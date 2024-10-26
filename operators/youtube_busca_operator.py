00e7d7335e91851c79c4c0259149adf23b152451
rodrigorocha1 <silva.rodrigo31@outlook.com>
2024-10-26 18:03:42 -0300

    operators

---
+++ operators/youtube_busca_operator.py
@@ -0,0 +1,6 @@
+from operators.youtube_operator import YoutubeOperator
+
+
+class YoutubeBuscaOperator(YoutubeOperator):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
---
+++ operators/youtube_operator.py
@@ -0,0 +1,20 @@
+from airflow.models import BaseOperator
+from abc import ABC, abstractmethod
+from src.dados.ioperacoes_dados import IoperacaoDados
+from hook.youtube_hook import YotubeHook
+from typing import Dict
+
+
+class YoutubeOperator(BaseOperator, ABC):
+    def __init__(self, **kwargs):
+        self.__operacao_dados = IoperacaoDados()
+        self.__operacao_hook = YotubeHook()
+        super().__init__(**kwargs)
+
+    @abstractmethod
+    def gravar_dados(self, req: Dict):
+        pass
+
+    @abstractmethod
+    def execute(self, context):
+        pass
