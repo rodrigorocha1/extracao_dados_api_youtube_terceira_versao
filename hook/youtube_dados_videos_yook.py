9dcd8db7177024ef30e6a05492886e95f14f3934
rodrigorocha1 <silva.rodrigo31@outlook.com>
2024-10-27 17:09:12 -0300

    lógica para gerar o params

---
+++ hook/youtube_dados_videos_yook.py
@@ -0,0 +1,37 @@
+
+from hook.youtube_hook import YotubeHook
+from src.dados.ioperacoes_dados import IoperacaoDados
+
+
+class YoutubeBuscaVideoHook(YotubeHook):
+    def __init__(self, conn_id, carregar_canais_brasileiros: IoperacaoDados, carregar_dados=None):
+        self.__carregar_canais_brasileiros = carregar_canais_brasileiros
+        super().__init__(conn_id, carregar_dados)
+
+    def _criar_url(self):
+        return self._URL + '/videos/'
+
+    def run(self):
+        session = self.get_conn()
+        lista_canais_brasileiros = self.__carregar_canais_brasileiros.carregar_dados()
+        lista_canais_video = self._carregar_dados.carregar_dados()
+        url = self._criar_url()
+        params = []
+        for canal_video in lista_canais_video:
+            if canal_video[1] in lista_canais_brasileiros:
+                param = {
+                    'part':  'statistics,contentDetails,id,snippet,status',
+                    'id': canal_video[0],
+                    'key': self._CHAVE,
+                    'regionCode': 'BR',
+                    'pageToken': ''
+
+                }
+                params.append(param)
+
+        response = self._executar_paginacao(
+            url=url,
+            session=session,
+            params=params
+        )
+        return response
