5a1561f679bcd391008c900f65497a6816c10dc0
rodrigorocha1 <silva.rodrigo31@outlook.com>
2024-11-16 12:53:19 -0300

    formatação

--- app.py
+++ app.py
@@ -40,16 +40,61 @@ def main():
     with st.container():
         st.write('Análse canais')
 
-        canal = dc.listar_canais_assunto(assunto, 1)
-        st.write(canal)
-    canal2 = dc.listar_canais_assunto(assunto, 2)
+        col1, col2 = st.columns(2)
+        with col1:
+            st.markdown(
+                """
+                    <div style="text-align: center;">
+                        <strong>Total visualizações/inscritos/vídeos publicados turno canal</strong>
+                    </div>
+                """,
+                unsafe_allow_html=True
+            )
+            tab1, tab2, tab3 = st.tabs(
+                ['Total vísualizações', 'Total Inscritos', 'Total Vídeo'])
 
-    st.write(canal2)
+            with tab1:
+                st.write('Total vísualizações')
+
+            with tab2:
+                st.write('Total Inscritos')
+
+            with tab3:
+                st.write('Total Vídeo')
+
+        with col2:
+            st.write(
+                '# 2 - Total vísualizações/inscritos/videos_publicado por dia canal'
+            )
 
     with st.container():
         st.write('Análise vídeo')
 
-    with
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.write(
+                '# 3-  TOTAL Visualizações, total comentários e total_likes vídeo Turno '
+            )
+        with col2:
+            st.write(
+                '  # 4 - Total Visualizações, total comentários e total_likes  vídeo dia'
+            )
+
+        with col3:
+            st.write(
+                ' # 5 - Média da taxa de engajamento do vídeo por dia'
+            )
+
+    with st.container():
+        st.write('Análise Taxa engajamento')
+
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.write(' -Média engajamento  do  canal por visualização')
+
+        with col2:
+            st.write('8 Fequência de vídeos publicados por assunto')
 
 
 main()
